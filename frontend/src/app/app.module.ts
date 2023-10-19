64ae1b0b4619debf5500b4a5d3932f5af0fb3205
Daniel Caballero <danielcaballero88@gmail.com>
2023-10-19 14:43:34 +0200

    Apply prettier project wide

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,21 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { MainComponent } from './components/main/main.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppMaterialModule } from 'src/app/app-material/app-material.module';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { MainComponent } from './components/main/main.component'
+import { HeaderComponent } from './components/header/header.component'
+import { FooterComponent } from './components/footer/footer.component'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { AppMaterialModule } from 'src/app/app-material/app-material.module'
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MainComponent,
-    HeaderComponent,
-    FooterComponent,
-  ],
+  declarations: [AppComponent, MainComponent, HeaderComponent, FooterComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -23,6 +18,6 @@ import { AppMaterialModule } from 'src/app/app-material/app-material.module';
     AppMaterialModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
