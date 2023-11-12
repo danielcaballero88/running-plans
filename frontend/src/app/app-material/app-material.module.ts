9dca3d6257a2aaa5be07dce65c3ad6f05bfac822
Daniel Caballero <danielcaballero88@gmail.com>
2023-11-12 10:44:21 +0100

    Import material components/modules

--- frontend/src/app/app-material/app-material.module.ts
+++ frontend/src/app/app-material/app-material.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon'
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { BrowserModule } from '@angular/platform-browser'
+
 @NgModule({
   declarations: [],
   imports: [CommonModule],
+  exports: [
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    BrowserModule,
+  ],
 })
 export class AppMaterialModule {}
