e46ad4038173b4cbd79fc06f0eb9e1174c0adcd5
Daniel Caballero <danielcaballero88@gmail.com>
2023-11-18 09:27:25 +0100

    Sidenav working

--- frontend/src/app/app-material/app-material.module.ts
+++ frontend/src/app/app-material/app-material.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { BrowserModule } from '@angular/platform-browser'
 
 import { MatButtonModule } from '@angular/material/button'
 import { MatIconModule } from '@angular/material/icon'
+import { MatListModule } from '@angular/material/list'
 import { MatSidenavModule } from '@angular/material/sidenav'
 import { MatToolbarModule } from '@angular/material/toolbar'
-import { BrowserModule } from '@angular/platform-browser'
 
 @NgModule({
   declarations: [],
@@ -13,6 +14,7 @@ import { BrowserModule } from '@angular/platform-browser'
   exports: [
     MatButtonModule,
     MatIconModule,
+    MatListModule,
     MatSidenavModule,
     MatToolbarModule,
     BrowserModule,
--- frontend/src/app/components/navigation/navigation.component.ts
+++ frontend/src/app/components/navigation/navigation.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core'
+import { Component, ViewChild } from '@angular/core'
+import { MatSidenav } from '@angular/material/sidenav'
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss'],
 })
-export class NavigationComponent {}
+export class NavigationComponent {
+  @ViewChild('sidenav') sidenav!: MatSidenav
+
+  // ngAfterViewInit () {
+  //   this.toggleSidenav()
+  // }
+
+  toggleSidenav() {
+    console.log('toggleSidenav')
+    console.log(this.sidenav)
+    this.sidenav.toggle()
+  }
+}
