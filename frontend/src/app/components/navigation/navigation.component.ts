5aa3c698ccd9d9c1a988ea0fc1b145abb87aa62a
Daniel Caballero <danielcaballero88@gmail.com>
2023-12-03 09:58:57 +0100

    Merge squash feature/frontend/improve-sidenav-with-animations
    Added animations to the background when opening sidenav so the background
    is dimmmed with a background color semi transparent.
    Also improved a bit the styling of the sidenav.

--- frontend/src/app/components/navigation/navigation.component.ts
+++ frontend/src/app/components/navigation/navigation.component.ts
@@ -1,13 +1,48 @@
 import { Component, ViewChild } from '@angular/core'
 import { MatSidenav } from '@angular/material/sidenav'
+import { trigger, state, style, animate, transition } from '@angular/animations'
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss'],
+  animations: [
+    trigger('triggerColor', [
+      state(
+        'colored',
+        style({
+          backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        })
+      ),
+      state(
+        'transparent',
+        style({
+          backgroundColor: 'rgba(0, 0, 0, 0)',
+        })
+      ),
+      transition('transparent <=> colored', [animate('200ms ease-in-out')]),
+    ]),
+    trigger('triggerVisibility', [
+      state(
+        'visible',
+        style({
+          visibility: 'visible',
+        })
+      ),
+      state(
+        'hidden',
+        style({
+          visibility: 'hidden',
+        })
+      ),
+      transition('hidden => visible', [animate('0s')]),
+      transition('visible => hidden', [animate('200ms')]),
+    ]),
+  ],
 })
 export class NavigationComponent {
   @ViewChild('sidenav') sidenav!: MatSidenav
+  isSidenavOpen: boolean = false
 
   // ngAfterViewInit () {
   //   this.toggleSidenav()
@@ -17,5 +52,6 @@ export class NavigationComponent {
     console.log('toggleSidenav')
     console.log(this.sidenav)
     this.sidenav.toggle()
+    this.isSidenavOpen = !this.isSidenavOpen
   }
 }
