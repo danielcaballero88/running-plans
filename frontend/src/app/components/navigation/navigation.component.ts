0afedcb14a09cebb9507d773a1acd565694fa53f
Daniel Caballero <danielcaballero88@gmail.com>
2023-12-03 09:57:08 +0100

    Add animation to background when open menu

--- frontend/src/app/components/navigation/navigation.component.ts
+++ frontend/src/app/components/navigation/navigation.component.ts
@@ -1,10 +1,44 @@
 import { Component, ViewChild } from '@angular/core'
-import { MatSidenav, MatSidenavContainer } from '@angular/material/sidenav'
+import { MatSidenav } from '@angular/material/sidenav'
+import { trigger, state, style, animate, transition } from '@angular/animations'
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss'],
+  animations: [
+    trigger('triggerColor', [
+      state(
+        'colored',
+        style({
+          backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        })
+      ),
+      state(
+        'transparent',
+        style({
+          backgroundColor: 'rgba(0, 0, 0, 0)',
+        })
+      ),
+      transition('transparent <=> colored', [animate('200ms ease-in-out')]),
+    ]),
+    trigger('triggerVisibility', [
+      state(
+        'visible',
+        style({
+          visibility: 'visible',
+        })
+      ),
+      state(
+        'hidden',
+        style({
+          visibility: 'hidden',
+        })
+      ),
+      transition('hidden => visible', [animate('0s')]),
+      transition('visible => hidden', [animate('200ms')]),
+    ]),
+  ],
 })
 export class NavigationComponent {
   @ViewChild('sidenav') sidenav!: MatSidenav
