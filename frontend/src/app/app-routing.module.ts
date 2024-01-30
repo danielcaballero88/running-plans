e87e518b340da560cdf4feda865fd9ef413776c8
Daniel Caballero <danielcaballero88@gmail.com>
2024-01-30 15:13:43 +0100

    Add route for running plan

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router'
 import { GetStartedComponent } from 'src/app/components/get-started/get-started.component'
 import { HomeComponent } from 'src/app/components/home/home.component'
 import { PaceChartComponent } from 'src/app/components/pace-chart/pace-chart.component'
+import { RunningPlanComponent } from 'src/app/components/running-plan/running-plan.component'
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'get-started', component: GetStartedComponent },
   { path: 'pace-chart', component: PaceChartComponent },
+  { path: 'running-plan', component: RunningPlanComponent },
 ]
 
 @NgModule({
